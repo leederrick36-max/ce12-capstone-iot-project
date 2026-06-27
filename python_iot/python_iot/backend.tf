0340cfe12f66ec95f72a2acac4cec16d961cd9e7
Shen Yean <yeosy@live.com>
2026-06-27 11:51:22 +0800

    removed IOT_Device apologise

--- python_iot/python_iot/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce12-tfstate-bucket"
-    key    = "kuankm/Iot/terraform.tfstate"
-    region = "ap-southeast-1"
-  }
-}
-
-
--- python_iot/python_iot/output.tf
+++
@@ -1,3 +0,0 @@
-output "iot_data_endpoint" { value = data.aws_iot_endpoint.data.endpoint_address }
-output "iot_credential_endpoint" { value = data.aws_iot_endpoint.creds.endpoint_address }
-output "s3_bucket_name" { value = aws_s3_bucket.simulation_data.id }
